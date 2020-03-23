334323b56738545832f8a5f70e27fd5aee5d5226
qli74 <13136985+qli74@users.noreply.github.com>
2020-03-23 13:02:37 -0400

    Update files

--- parlai/agents/transformer/generatorMMI.py
+++ parlai/agents/transformer/generatorMMI.py
@@ -93,6 +93,7 @@ class GeneratorMMIAgent(TransformerGeneratorAgent):
     def __init__(self, opt, shared=None):
         super().__init__(opt, shared)
         opt_inv=deepcopy(self.opt)
+        opt_inv['model'] = 'transformer/generatorMMI'
         opt_inv['model_file'] = self.opt['model_file']+'_inv'
         opt_inv['override']['model_file'] = self.opt['model_file']+'_inv'
         self.model_inv=TransformerGeneratorModel(opt_inv, self.dict)
