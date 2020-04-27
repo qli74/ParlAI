3173fa98fe3a01695301f0ca14cfcca33938a85f
qli74 <13136985+qli74@users.noreply.github.com>
2020-04-27 14:15:25 -0400

    Upload files

--- fastapi_covid.py
+++ fastapi_covid.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+##########################################################
+# Launch a command with pipes
 p = subprocess.Popen(['python -m parlai.scripts.interactive -mf ../model/poly/covid7'], shell=True,
                      stdout=subprocess.PIPE,
                      stdin=subprocess.PIPE)
@@ -13,20 +15,18 @@ while 1:
     #print(line)
     if line == '[  polyencoder_type: codes ]':
         break
-
+#the model is ready for interactivation
+##########################################################
 @app.get("/")
 def root(question: str = "What is Covid-19?"):
-    # Send the data and get the output
+    # Send the question and get the output
     p.stdin.write(bytes(question, 'utf-8'))
     p.stdin.write(bytes("\n", 'utf-8'))
     p.stdin.flush()
-    # To interpret as text, decode
     line = p.stdout.readline()
-    # p.stdout.flush()
-    line = line.strip().decode()
-
-    if '[Polyencoder]' in line:
+    line = line.strip().decode() # To interpret as text, decode
+    if '[Polyencoder]' in line: # Exclude warnings and other messages
         line = line.split('[Polyencoder]:')
-    print(line)
+    #print(line)
     result=line[-1]
     return {"question": question, "answers": result}
