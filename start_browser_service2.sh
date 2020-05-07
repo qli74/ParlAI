b68d799bcf4b56f86ff9b41a376cfbbc47cf5395
qli74 <13136985+qli74@users.noreply.github.com>
2020-05-07 01:50:21 -0400

    Upload files

--- start_browser_service2.sh
+++ start_browser_service2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python parlai/chat_service/services/browser_chat2/run.py    --config-path  parlai/chat_service/tasks/chatbot/config2.yml --port 15678 &
-sleep 15
-python parlai/chat_service/services/browser_chat2/client.py  --port 15678
+python parlai/chat_service/services/browser_chat2/run.py    --config-path  parlai/chat_service/tasks/chatbot/config2.yml --port 1678 &
+sleep 20
+python parlai/chat_service/services/browser_chat2/client.py  --port 1678
