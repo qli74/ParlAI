cd44fd54718b276210017e9efb0af0f0c3b5c27c
qli74 <13136985+qli74@users.noreply.github.com>
2020-05-07 02:06:09 -0400

    Upload files

--- start_browser_service.sh
+++ start_browser_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python parlai/chat_service/services/browser_chat/run.py --config-path  parlai/chat_service/tasks/chatbot/config.yml &
-sleep 10
+sleep 20
 python parlai/chat_service/services/browser_chat/client.py
